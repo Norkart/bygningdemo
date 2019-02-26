52d887bf909d1429690ee9620f659745b7d13e9e
Dhruba Dahal <dhrdah@norkart.no>
2019-02-26 10:04:13 +0100

    Change One

--- building-client/src/App.js
+++ building-client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import FeatureComponent from './FeatureComponent';
 class App extends Component {
   render() {
     return (
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <FeatureComponent name="Iechha"/>
           </p>
           <a
             className="App-link"
@@ -17,9 +17,9 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
           </a>
         </header>
+        
       </div>
     );
   }
