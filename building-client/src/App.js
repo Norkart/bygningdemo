8ae1365b5922db0774613eb88f12c1d9f3812657
Nicklas Eltvik <nicelt@norkart.no>
2019-03-15 15:16:48 +0100

    fix icons

--- building-client/src/App.js
+++ building-client/src/App.js
@@ -5,17 +5,45 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import ApiKeyInput from './components/ApiKeyInput';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBuilding } from '@fortawesome/free-solid-svg-icons'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
-import { faWarehouse } from '@fortawesome/free-solid-svg-icons'
-import { faTrain } from '@fortawesome/free-solid-svg-icons'
+import ApiKeyInput from "./components/ApiKeyInput";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBuilding,
+  faHome,
+  faWarehouse,
+  faTrain,
+  faLandmark,
+  faHotel,
+  faBriefcase,
+  faClinicMedical,
+  faTh,
+  faSchool,
+  faStoreAlt,
+  faStore,
+  faHospitalAlt,
+  faPlaceOfWorship,
+  faMonument
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(faBuilding);
 library.add(faHome);
 library.add(faWarehouse);
 library.add(faTrain);
+library.add(faLandmark);
+library.add(faHotel);
+library.add(faBriefcase);
+library.add(faClinicMedical);
+library.add(faHospitalAlt);
+library.add(faTh);
+library.add(faStoreAlt);
+library.add(faSchool);
+library.add(faStore);
+library.add(faMonument);
+
+library.add(faPlaceOfWorship);
+
+
+
 
 
 
@@ -23,17 +51,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <ContentWrapper>
-            <Header />
-            <Sidebar classNames={"sidebarLeft"}>
+        <ContentWrapper>
+          <Header />
+          <Sidebar classNames={"sidebarLeft"}>
             <ApiKeyInput />
-            </Sidebar>
-            <MainContent
-              searchboxLabel="Søk på gateadresse:"
-            />
-            <Sidebar classNames={"sidebarRight"}/>
-            <Footer />
-          </ContentWrapper>
+          </Sidebar>
+          <MainContent searchboxLabel="Søk på gateadresse:" />
+          <Sidebar classNames={"sidebarRight"} />
+          <Footer />
+        </ContentWrapper>
       </div>
     );
   }
--- building-client/src/components/BuildingList.js
+++ building-client/src/components/BuildingList.js
@@ -88,16 +88,43 @@ class BuildingList extends Component {
   }
 
   getFontIcon(bygningstype) {
-    console.log(bygningstype);
     switch (bygningstype) {
-      case "Enebolig":
+      case "Rekkehus":
         return "home";
       case "Jernbane- og T-banestasjon":
         return "train";
       case "Garasjeuthus anneks til bolig":
-        return "building";
+        return "warehouse";
+      case "Kontor- og adm.bygning rådhus":
+        return "landmark";
+      case "Hotellbygning":
+        return "hotel";
+      case "Annen kontorbygning":
+        return "briefcase";
+      case "Bo- og behandlingssenter":
+        return "clinic-medical";
+      case "Sykehjem":
+        return "clinic-medical";
+      case "Sykehus":
+        return "hospital-alt";
+      case "Lagerhall":
+        return "th";
+      case "Barnehage":
+        return "school";
+      case "Butikk/forretningsbygning":
+        return "store-alt";
+      case "Kirkesogn":
+        return "place-of-worship";
+      case "Kloster":
+        return "place-of-worship";
+      case "Kirke kapell":
+        return "place-of-worship";
+      case "Monument":
+        return "monument";
       default:
-        return "home";
+        if (bygningstype.toLowerCase().includes("bolig")) return "home";
+        else if (bygningstype.toLowerCase().includes("skole")) return "school";
+        return "building";
     }
   }
   BuildingTextList(building) {
@@ -107,7 +134,7 @@ class BuildingList extends Component {
           <div onClick={e => this.GetDetail(item)}>
             <FontAwesomeIcon
               icon={this.getFontIcon(item.MatrikkelData.Bygningstype)}
-              size="lg"
+              size="3x"
             />
             <div>{item.MatrikkelData.Bygningstype}</div>
           </div>
